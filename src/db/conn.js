8ec20bd5a7048b707b9638874cf346f4a5d4861c
itsmesaim <saimkaskar69@gmail.com>
2024-04-08 10:17:30 +0530

    updated mongodb

--- src/db/conn.js
+++ src/db/conn.js
@@ -6,8 +6,10 @@ const mongoose = require('mongoose');
 exports.connectMongoose = () => {
     mongoose.set("strictQuery", false);
 
-    mongoose.connect('mongodb+srv://saimkaskar786:72LrqWfakRAnzY75@habittrackerdb.qlwcdne.mongodb.net/?retryWrites=true&w=majority&appName=HabitTrackerDB')
-        // mongoose.connect('mongodb://127.0.0.1:27017/HabitTracker', { useNewUrlParser: true })
-        .then((e) => console.log("Connected to Mongodb => Habit-Tracker"))
-        .catch((e) => console.log("Not Connect Mongodb", e));
+    mongoose.connect("mongodb+srv://saimkaskar786:72LrqWfakRAnzY75@habittrackerdb.qlwcdne.mongodb.net/?retryWrites=true&w=majority&appName=HabitTrackerDB")
+        .then(() => {
+            console.log(`Successfully Connected to Mongodb`);
+        }).catch((e) => {
+            console.log("Unable to Connect to Mongodb ", e);
+        })
 }
