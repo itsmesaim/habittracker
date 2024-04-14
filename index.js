a9c28aca5de58489ac657fd2c1f38892fd551c41
itsmesaim <saimkaskar69@gmail.com>
2024-04-15 00:35:03 +0530

    port update

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use(express.json())
 
 // Setting up routes
 app.use(router);
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server running at port: ${PORT}`);
 })
